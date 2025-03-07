f986bbaef5fe3196c11bee59470103c95076a05a
Florian Schmidt <nme199911@gmail.com>
2025-03-07 11:22:30 +0100

    created script to annotate vcf file from snpeff db

---
+++ batch_snpeff_annotate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+#SBATCH --job-name=snpAnno
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=0-01:00:00
+#SBATCH --partition=basic,short
+#SBATCH --output=/lisc/scratch/botany/frschmidt/mk_test/logs/snpEff_annotate%j.log
+#SBATCH --error=/lisc/scratch/botany/frschmidt/mk_test/logs/snpEff_annotate%j.err
+#SBATCH --mail-type=END
+
+# ENVIRONMENT 
+ml java
+ml htslib
+SNPEFF_DIR=/lisc/scratch/botany/frschmidt/bin/snpEff
+SNPEFF=${SNPEFF_DIR}/snpEff.jar
+
+module list
+java -jar $SNPEFF -version
+
+# VARIABLES
+WD=/lisc/scratch/botany/frschmidt/mk_test
+
+# snpEff dirs and files
+annot_version="vie.refAnnot"
+SNPEFF_CONFIG=${SNPEFF_DIR}/snpEff.config 
+db_dir=${WD}/data/snpEff_DB/
+
+# Calciphila and sp PicN'ga vcf-file
+data_dir=${WD}/data/variants
+vcf=${data_dir}/pair_cal_spn/vieref_cal_spn.vcf.gz
+
+# output directory and annotated vcf-file
+out_dir=${WD}/raw_data/annotation
+vcf_ann=${out_dir}/$(basename ${vcf%.vcf.gz}).ann.vcf.gz
+
+# EXECUTION
+mkdir -p ${data_dir}
+mkdir -p ${out_dir}
+
+# run snpeff on specified vcf file
+java -Xmx8g -jar ${SNPEFF} ${annot_version} -dataDir ${db_dir} ${vcf} | bgzip -c > ${vcf_ann}
